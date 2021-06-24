8d4661aa8819aeb8b4cb20007c8b8a85a6d7e7f2
Brandon <brandonspersonalmailbox@gmail.com>
2021-06-24 11:09:24 -0400

    onDigit and onClear functionality implemented

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
     fun onDigit(view: View){
-        Toast.makeText(this, "Button Works", Toast.LENGTH_LONG).show()
+        tvInput.append((view as Button).text)
     }
+    fun onClear(view : View){
+        tvInput.text = ""
+    }
+
 }
