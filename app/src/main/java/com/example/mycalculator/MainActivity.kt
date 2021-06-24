7f3ff2e09ffb2eb23b2f615e8a680f6ddfbd4466
Brandon <brandonspersonalmailbox@gmail.com>
2021-06-24 11:53:11 -0400

    onEqual started -> implemented code for subtraction

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -49,6 +50,39 @@ class MainActivity : AppCompatActivity() {
             false
         } else {
             value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+        }
+    }
+
+    fun onEqual (view: View) {
+        if (lastNumeric){
+            var tvValue = tvInput.text.toString() //storing the calculator text into new var
+            var prefix = "" //empty prefix
+
+            try {
+
+                if (tvValue.startsWith("-")){ //checks if there is a prefix
+                    prefix = "-" //sets it
+                    tvValue = tvValue.substring(1) //creating a substring starting at the index 1 till the end of the string
+
+                }
+
+                if(tvValue.contains("-")) {
+
+                    val splitValue = tvValue.split("-") //acts like a array with the split values
+                    var one = splitValue[0] //position 1
+                    var two = splitValue[1] //position 2
+
+                    if (!prefix.isEmpty()){ //adding the prefix of - back to the position 1 -- AFTER THE SPLIT
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+
+            }catch(e: ArithmeticException){
+                e.printStackTrace()
+            }
         }
     }
 
